cd32cf714ac6fb60fd1a4a134468adfb993f0c3f
CHO HYE RA <s2037@e-mirim.hs.kr>
2021-11-30 19:52:42 +0900

    background image안 보임

--- game/src/SnowCastle/background.java
+++ game/src/SnowCastle/background.java
@@ -3,26 +3,49 @@ package SnowCastle;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class background extends JFrame {
 	
-	public static void main(String[] args){
-		
-		JFrame f = new JFrame("SnowCastle");
-		f.setSize(1600, 900);
-		f.setVisible(true);
-		f.setResizable(false);//창의 크기를 변경하지 못하게
-		f.setLocationRelativeTo(null);//창이 가운데 나오게
-		
-		// 스윙에서 창 닫기 싱행 정의
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// ImageIcon image = new ImageIcon("../src/image/back.png");
-		// 주석해보기
-		f.getContentPane().setBackground(Color.black);
-		/*JLabel jla = new JLabel(image);
-		jla.setSize(1600, 900);
-		f.add(jla);*/
-		//jla.setVisible(true);
+    JScrollPane scrollPane;
+    ImageIcon icon;
+    
+    public background() {
+
+	    // 생성자에 ico 호출 하고 이미지 경로 지정...(상대경로로 안될경우 절대경로 지정)
+    	ImageIcon ii = new ImageIcon(this.getClass().getResource("winter_back.png"));
+	
+	   // 백그라운드 이미지 삽입할 메소드에 이름없는 클래스로 구현
+	    JPanel panel = new JPanel() {
+	        public void paintComponent(Graphics g) {
+	            //  Approach 1: Dispaly image at at full size
+	            g.drawImage(icon.getImage(), 0, 0, null);
+	            //  Approach 2: Scale image to size of component
+	            // Dimension d = getSize();
+	            // g.drawImage(icon.getImage(), 0, 0, d.width, d.height, null);
+	            // Approach 3: Fix the image position in the scroll pane
+	            // Point p = scrollPane.getViewport().getViewPosition();
+	            // g.drawImage(icon.getImage(), p.x, p.y, null);
+	            setOpaque(false);
+	            super.paintComponent(g);
+	        }
+	    };
+	    //JButton button = new JButton("Hello");
+	    //panel.add(button);
+	    scrollPane = new JScrollPane(panel);
+	    setContentPane(scrollPane);
+	}
+	
+	public static void main(String[] args) {
+		background frame = new background();
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setSize(1920, 1080);
+	    frame.setVisible(true);
 	}
 }
+
+

d012e44041b85ec40750d5ec4dff87883bad8b57
CHO HYE RA <s2037@e-mirim.hs.kr>
2021-12-01 00:48:15 +0900

    startImageButton 추가

--- game/src/SnowCastle/background.java
+++ game/src/SnowCastle/background.java
@@ -1,32 +1,52 @@
 package SnowCastle;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.net.URL;
-
+import javax.swing.*;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class background extends JFrame {
 	
 	public static void main(String[] args) {
 		
-		// 새 창 띄우기
-		JFrame frame = new JFrame("SnowCastle");
-		frame.setSize(1920,1080);//프레임의 크기
-		frame.setResizable(false);//창의 크기를 변경하지 못하게
-		frame.setLocationRelativeTo(null);//창이 가운데 나오게
-		frame.setLayout(null);//레이아웃을 내맘대로 설정가능하게 해줌.
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//JFrame이 정상적으로 종료되게
+		String imgPath = "../game/src/SnowCastle/startBtn.png";
 		
+		ImageIcon startbtnImage = new ImageIcon(imgPath);
 		
-		// 처음 스플래쉬 화면 띄우기
-		ImageIcon ii = new ImageIcon("../game/src/SnowCastle/winterBack1.png");
-		JLabel jl = new JLabel(ii);
-		jl.setSize(1920,1080);
-		frame.add(jl);
-		frame.setLayout(null);
-		frame.setVisible(true);
+		//ImageIcon에서 Image를 추출
+		Image originImg = startbtnImage.getImage(); 
+		
+		//추출된 Image의 크기를 조절하여 새로운 Image객체 생성
+		Image changedImg= originImg.getScaledInstance(400, 180, Image.SCALE_SMOOTH );
+
+		//새로운 Image로 ImageIcon객체를 생성
+		ImageIcon startbtn = new ImageIcon(changedImg);
+
+		
+		JLabel Background = new JLabel(new ImageIcon("../game/src/SnowCastle/winterBack1.png"));
+		JButton jbutton = new JButton(startbtn);
+		JFrame jframe = new JFrame();
+		
+		// 화면 해상도 구하기
+		Dimension res = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		jframe.add(Background);
+		jframe.setSize(res.width, res.height);
+		jframe.setVisible(true);
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setLayout(null);
+		
+		jbutton.setBounds(200, 200, 500, 150);
+		jbutton.setSize(400, 180);
+		jframe.add(jbutton);
 		
 
 	}

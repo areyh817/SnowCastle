ea344a7e723eb3039f8058679e96a1ef00739657
CHO HYE RA <s2037@e-mirim.hs.kr>
2021-11-30 20:03:41 +0900

    background image 추가

--- game/src/SnowCastle/background.java
+++ game/src/SnowCastle/background.java
@@ -1,50 +1,28 @@
 package SnowCastle;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class background extends JFrame {
 	
-    JScrollPane scrollPane;
-    ImageIcon icon;
-    
-    public background() {
-
-	    // 생성자에 ico 호출 하고 이미지 경로 지정...(상대경로로 안될경우 절대경로 지정)
-    	ImageIcon ii = new ImageIcon(this.getClass().getResource("winter_back.png"));
-	
-	   // 백그라운드 이미지 삽입할 메소드에 이름없는 클래스로 구현
-	    JPanel panel = new JPanel() {
-	        public void paintComponent(Graphics g) {
-	            //  Approach 1: Dispaly image at at full size
-	            g.drawImage(icon.getImage(), 0, 0, null);
-	            //  Approach 2: Scale image to size of component
-	            // Dimension d = getSize();
-	            // g.drawImage(icon.getImage(), 0, 0, d.width, d.height, null);
-	            // Approach 3: Fix the image position in the scroll pane
-	            // Point p = scrollPane.getViewport().getViewPosition();
-	            // g.drawImage(icon.getImage(), p.x, p.y, null);
-	            setOpaque(false);
-	            super.paintComponent(g);
-	        }
-	    };
-	    //JButton button = new JButton("Hello");
-	    //panel.add(button);
-	    scrollPane = new JScrollPane(panel);
-	    setContentPane(scrollPane);
-	}
-	
 	public static void main(String[] args) {
-		background frame = new background();
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setSize(1920, 1080);
-	    frame.setVisible(true);
+		
+		// 새 창 띄우기
+		JFrame frame = new JFrame("SnowCastle");
+		frame.setSize(1920,1080);//프레임의 크기
+		frame.setResizable(false);//창의 크기를 변경하지 못하게
+		frame.setLocationRelativeTo(null);//창이 가운데 나오게
+		frame.setLayout(null);//레이아웃을 내맘대로 설정가능하게 해줌.
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//JFrame이 정상적으로 종료되게
+		
+		// 처음 스플래쉬 화면 띄우기
+		ImageIcon ii = new ImageIcon("../game/src/SnowCastle/winter_back.png");
+		JLabel jl = new JLabel(ii);
+		jl.setSize(1920,1080);
+		frame.add(jl);
+		frame.setLayout(null);
+		frame.setVisible(true);
 	}
 }
 
